3226c6152e27e66be60e58feaf2e02c8ea0034cb
TOMBAY <akashtambe11@gmail.com>
2020-01-05 15:49:19 +0530

    Upload: Logical Programms

--- src/com/BridgeLabz/Fellowship/Logical/Addition.java
+++ src/com/BridgeLabz/Fellowship/Logical/Addition.java
@@ -4,8 +4,9 @@ import com.BridgeLabz.Fellowship.Utility.Utility;
 
 public class Addition {
 public static void main(String[] args) {
-	System.out.println("Enter two number");
+	System.out.println("Enter one number");
 	int a = Utility.integerInput();
+	System.out.println("Enter second number");
 	int b = Utility.integerInput();
 	
 	Utility.add(a, b);
